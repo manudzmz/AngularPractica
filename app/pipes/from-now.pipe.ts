976bb8f0d5320a65e9907de39363611fd23abf11
Manuel <manudzmz@gmail.com>
2016-11-17 14:15:36 +0100

    Añadido Blue Path (transformación de la fecha de publicación)

---
+++ app/pipes/from-now.pipe.ts
@@ -0,0 +1,23 @@
+import * as moment from "moment";
+import "moment/locale/es";
+
+import { Component, Pipe, PipeTransform } from '@angular/core';
+ 
+
+/*---------------------------------------------------------------------------------------------------------|
+ | ~~~ Blue Path ~~~                                                                                       |
+ |---------------------------------------------------------------------------------------------------------|
+ | Crea el pipe FromNowPipe. Su cometido es, partiendo de una fecha dada, retornar una cadena de texto     |
+ | que exprese el tiempo que ha pasado desde dicha fecha hasta ahora. Por ejemplo: hace 2 horas. Para esta |
+ | tarea nos apoyamos en la librería Moment.js; ya tienes hecho el import correspondiente, solo tienes que |
+ | usarla donde proceda. Haciendo 'moment(fecha).fromNow()' obtenemos justo lo que necesitamos.            |
+ |---------------------------------------------------------------------------------------------------------*/
+
+@Pipe({
+    name: "TransFecha"
+})
+export class TransFechaPipe implements PipeTransform {
+    transform(fecha: Date){
+        return moment(fecha).fromNow()
+    }
+}
